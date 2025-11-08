9c60442d71fa4d869d68022f6b53dcb9aef6b10a
gtanishka100 <gtanishka100@gmail.com>
2025-11-08 09:29:53 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import SignIn from "./components/SignIn";
+import "./index.css";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <SignIn />
     </div>
   );
